752e8a758c65a864d7aa113f08951b92ff8d4eae
marcrusch <marc.rusch@hotmail.ch>
2022-04-07 13:07:04 +0200

    Fix image sizes

--- src/utils/products.js
+++ src/utils/products.js
@@ -2,31 +2,31 @@ const products = [
   {
     title: 'MacBook Pro 16"',
     media:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-16-202110?wid=6&hei=5&fmt=p-jpg&qlt=95&.v=16337262440",
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mac-card-40-macbook-pro-16-202110",
     price: 2499,
   },
   {
     title: "iPhone 13 Pro",
     media:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13pro-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1644989935267",
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13pro-202203",
     price: 999,
   },
   {
     title: "iPhone 13",
     media:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646335268199",
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13-202203",
     price: 699,
   },
   {
     title: "iPhone SE",
     media:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=340&hei=264&fmt=jpeg&qlt=95&.v=1646415838921",
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphonese-202203",
     price: 429,
   },
   {
     title: "Xiaomi 12 Pro",
     media:
-      "https://i01.appmifile.com/webfile/globalimg/dongxuechun/12prohei.png?width=140&height=140",
+      "https://i01.appmifile.com/webfile/globalimg/dongxuechun/12prohei.png",
     price: 550,
   },
 ];
